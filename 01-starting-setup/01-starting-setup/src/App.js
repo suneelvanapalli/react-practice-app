be7cb043a399395c96250c2bd814395c562da74a
SuneelKumar Vanapalli <suneel.vanapalli@adudev.com>
2021-06-08 21:08:23 +1000

    initia changes

--- 01-starting-setup/01-starting-setup/src/App.js
+++ 01-starting-setup/01-starting-setup/src/App.js
@@ -1,9 +1,17 @@
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Header></Header>
+
+      <div>
+        <h2>Let's get started!</h2>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+export function Header() {
+  return <section>App header</section>;
+}
