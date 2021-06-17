c581c72ecd23a9ae5f0709285d357b6b81bede0b
SuneelKumar Vanapalli <suneel.vanapalli@adudev.com>
2021-06-17 21:19:58 +1000

    using es6 for React class comonent component life cycle methods using state for reactive data props

--- 01-starting-setup/01-starting-setup/src/App.js
+++ 01-starting-setup/01-starting-setup/src/App.js
@@ -1,17 +1,48 @@
-function App() {
-  return (
-    <div>
-      <Header></Header>
+import React from 'react';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.tick();
+    }, 1000);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    return (
       <div>
-        <h2>Let's get started!</h2>
+        <Header name='My (Not) First React App'></Header>
+
+        <div>
+          <h2>Let's get started!</h2>
+          <p>Time is {this.state.date.toLocaleTimeString()}</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
 
-export function Header() {
-  return <section>App header</section>;
+export class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <section>{this.props.name}</section>;
+  }
 }
